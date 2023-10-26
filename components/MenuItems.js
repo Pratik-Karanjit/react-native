89f4324859b51af0a2ce86b7009ce7a14f7ff63a
Pratik Karanjit <pratikkaranjit@gmail.com>
2023-10-26 21:35:56 +0545

    comments

--- components/MenuItems.js
+++ components/MenuItems.js
@@ -8,10 +8,12 @@ const menuItemsToDisplay = [
     'Samosa \n Paneer Tikka \n Butter Chicken \n Biryani \n Chana Masala \n Tandoori Chicken \n Rogan Josh \n Aloo Gobi \n Naan Bread \n Palak Paneer \n Dosa \n Malai Kofta \n Chicken Tikka Masala \n Lamb Vindaloo \n Vegetable Biryani \n Rogani Murgh \n Mango Lassi \n Gulab Jamun \n Raita \n Tandoori Roti'
   ];
 
+
 const MenuItems = () => {
   return (
     <View style={{ flex: 0.9 }}>
       <ScrollView
+      //Setting indicatorStyle to white keeps the color of the scroller into white.
         indicatorStyle= {"white"}
         style={{
           paddingHorizontal: 20,
