9d0e049ac31dabd937d12e058154757e2f515fa9
Pratik Karanjit <pratikkaranjit@gmail.com>
2023-11-02 15:26:50 +0545

    Styling Flatlist

--- components/MenuItems.js
+++ components/MenuItems.js
@@ -4,9 +4,9 @@ const menuItemsToDisplay = [
     { name: 'Hummus', price: '$5.00', id: '1A' },
     { name: 'Moutabal', price: '$5.00', id: '2B' },
     { name: 'Falafel', price: '$7.50', id: '3C' },
-    { name: 'Marinated Olives', price: '$5.00', id: '4D' },
+    { name: 'Marinated', price: '$5.00', id: '4D' },
     { name: 'Kofta', price: '$5.00', id: '5E' },
-    { name: 'Eggplant Salad', price: '$8.50', id: '6F' },
+    { name: 'Eggplant ', price: '$8.50', id: '6F' },
     { name: 'Lentil Burger', price: '$10.00', id: '7G' },
     { name: 'Smoked Salmon', price: '$14.00', id: '8H' },
     { name: 'Kofta Burger', price: '$11.00', id: '9I' },
@@ -22,26 +22,38 @@ const menuItemsToDisplay = [
     { name: 'Tartufo', price: '$3.00', id: '19T' },
     { name: 'Tiramisu', price: '$5.00', id: '20U' },
     { name: 'Panna Cotta', price: '$5.00', id: '21V' },
-  ];
 ];                                                     //menuItemsToDisplay has name and id
 
-const Item = ({ name }) => (                            //Item function takes the name prop from renderItem function to display name
+
+const Separator = () => 
+  <View style={menuStyles.separator}/>
+
+const Header = () => <Text style={menuStyles.headerText}>View Menu</Text>
+
+
+
+const Item = ({ name, price  }) => (                            //Item function takes the name prop from renderItem function to display name
   <View style={menuStyles.innerContainer}>
     <Text style={menuStyles.itemText}>{name}</Text>
+    <Text style={menuStyles.itemText}>{price}</Text>
   </View>
 );
 
 
 const MenuItems = () => {                               //this is the main function
-  const renderItem = ({ item }) => <Item name={item.name} />;                         //This renderItem function takes item objects from menuItemsToDisplay and passes name prop for Item function
+  const renderItem = ({ item }) => <Item name={item.name} price={item.price} />;                         //This renderItem function takes item objects from menuItemsToDisplay and passes name prop for Item function
 
   return (
     <View style={menuStyles.container}>
-      <Text style={menuStyles.headerText}>View Menu</Text>
       <FlatList                           //FlatList passes 3 properties: data, keyExtraction, renderItem. 
         data={menuItemsToDisplay}         //This takes all the data from menuItemsToDisplay
         keyExtractor={(item) => item.id}  //keyExtractor is used to specify a function that extracts a unique key for each item in the list.
-        renderItem={renderItem}></FlatList>        
+        renderItem={renderItem}
+        ItemSeparatorComponent={Separator}
+        ListHeaderComponent={Header}
+        >
+          
+          </FlatList>        
       {/* This renderItem displays the list of items */}
     </View>
 
@@ -57,6 +69,8 @@ const menuStyles = StyleSheet.create({
     paddingHorizontal: 40,
     paddingVertical: 20,
     backgroundColor: 'black',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   headerText: {
     color: 'white',
@@ -68,6 +82,10 @@ const menuStyles = StyleSheet.create({
     color: '#F4CE14',
     fontSize: 36,
   },
+  separator: {
+    borderBottomWidth:1,
+    borderColor: '#EDEFEE',
+  }
 });
 
 export default MenuItems;
